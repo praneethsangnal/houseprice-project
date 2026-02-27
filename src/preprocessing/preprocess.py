c3f2946ef700b93faab68b031b7788a66fa9b3f6
Praneeth Sangnal <praneethsangnal8305@gmail.com>
2026-02-28 00:15:40 +0530

    preprocessing completed

---
+++ src/preprocessing/preprocess.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+def preprocess(df):
+    #very basic preprocessing
+    #drop very useless col
+    #fill numeric cols with mean
+    #fill non numeric with none
+    #do one hot encoding
+    #replace saleprice with log values
+    df=df.drop("Id",axis=1)
+
+    X=df.drop("SalePrice",axis=1)
+    y=np.log1p(df["SalePrice"])
+    
+    num_cols = X.select_dtypes(
+        include=["int64", "float64"]
+    ).columns
+
+    cat_cols = X.select_dtypes(
+        include=["object", "string", "category"]
+    ).columns
+
+    X[num_cols]=X[num_cols].fillna(X[num_cols].median())
+
+    X[cat_cols]=X[cat_cols].fillna("None")
+
+    X=pd.get_dummies(X,drop_first=True)
+
+    return X,y
